0209de1269cc62d22bfb999c3eeb92d2922c9914
Dzhavid <eldarov.dzhavid@mail.ru>
2020-06-25 00:06:42 +0300

    Initial first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+
+public class Cat {
+    public Cat() {
+    }
+
+    private Cat cat;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+}
